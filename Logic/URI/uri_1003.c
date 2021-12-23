69b121a0cc5223092bb040808e5c30aa98ebc3fc
Matheus Sá Cardoso <78807235+matheussacardoso@users.noreply.github.com>
2021-12-22 21:02:51 -0300

    11 questões

---
+++ Logic/URI/uri_1003.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+	int age,days,month,year;
+	scanf("%d", &age);
+	if (age = 365){
+		printf("%d", &year);
+	}
+	
+	if (age = 30){
+		printf("%d", &month);	
+	}
+
+	if (age < 30){
+		printf("%d", &days);
+	}
+	
+    return 0;
+}
