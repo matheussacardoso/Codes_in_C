8776a7690e576571d79271db5db6903e984e9be8
Matheus Sá Cardoso <78807235+matheussacardoso@users.noreply.github.com>
2022-01-31 17:02:18 -0300

    Add files via upload

---
+++ Java/Guanabara-Aula06/src/guanabara/aula06/GuanabaraAula06.java
@@ -0,0 +1,13 @@
+package guanabara.aula06;
+
+public class GuanabaraAula06 {
+    
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+        c.ligarMudo();
+        c.abrirMenu();
+    }
+    
+}
